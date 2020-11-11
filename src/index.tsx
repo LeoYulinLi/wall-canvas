3eb6b799cce8825f5e26028bd17715b43f02dc96
LeoYulinLi <LeoYulinLi@gmail.com>
2020-11-10 21:00:47 -0800

    Added react router

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./components/app/App";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ App } />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
